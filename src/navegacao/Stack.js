4af14c7fdbe0eadf6059e1dd15519fe5edf8c54b
unknown <alanfelipe.neres@outlook.com>
2020-11-11 23:10:23 -0300

    first commit

---
+++ src/navegacao/Stack.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import { createStackNavigator } from '@react-navigation/stack'
+import TelaA from '../views/TelaA'
+import TelaB from '../views/TelaB'
+import TelaC from '../views/TelaC'
+import PassoStack from '../components/PassoStack'
+
+const Stack = createStackNavigator()
+
+export default props => (
+    <Stack.Navigator initialRouteName='TelaA' screenOptions={{ headerShown: true }}>
+
+        <Stack.Screen
+            options={{ title: 'Informações' }}
+            name='TelaA'>
+            {
+                props => (
+                    <PassoStack {...props} avancar='TelaB'>
+                        <TelaA />
+                    </PassoStack>
+                )
+            }
+        </Stack.Screen>
+
+        <Stack.Screen name='TelaB'>
+            {
+                props => (
+                    <PassoStack {...props} voltar avancar='TelaC'>
+                        <TelaB />
+                    </PassoStack>
+                )
+            }
+        </Stack.Screen>
+
+        <Stack.Screen name='TelaC'>
+            {
+                props => (
+                    <PassoStack {...props} voltar avancar='TelaC'>
+                        <TelaC {...props}/>
+                    </PassoStack>
+                )
+            }
+        </Stack.Screen>
+    </Stack.Navigator>
+)
